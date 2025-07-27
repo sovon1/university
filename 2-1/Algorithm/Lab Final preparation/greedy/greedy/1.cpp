a255a10aa554ed0a839f4a14ff07e72d37e74c1d
sovon1 <sovon2018@gmail.com>
2025-07-27 07:08:56 +0600

    practiced things added not important ignore

--- 2-1/Algorithm/Lab Final preparation/greedy/greedy/1.cpp
+++ 2-1/Algorithm/Lab Final preparation/greedy/greedy/1.cpp
@@ -1,29 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-        int deadline[]={3,1,2,2};
-    int prfit[]={50,10,20,30};
-    int n=sizeof(deadline)/sizeof(deadline[0]);
-    vector<pair<int,int>>v;
-    for(int i=0;i<n;i++){
-        v.push_back({prfit[i],deadline[i]});
+        int value []={60,100,120};
+    int weight[]={10,20,30};
+    int n=sizeof(value)/sizeof(value[0]);
+    int capacity =50;
+    vector<pair<double,pair<int,int>>> v;
+    for(int i=0;i<n;i++){   
+        v.push_back({(double)value[i]/weight[i],{weight[i],value[i]}});
     }
-    sort(v.begin(),v.end(),greater<pair<int,int>>());
-    int mx = 0;
+    sort(v.begin(),v.end(),greater<pair<double,pair<int,int>>>());
+    double totalvalue=0.0;
     for(int i=0;i<n;i++){
-        mx=max(mx,v[i].second);
-    }
-    vector<int> slot(mx+1,-1);
-    int cntjob=0;
-    int totalprofit=0;
-    for(int i=0;i<n;i++){
-        for(int j=v[i].second;j>0;j--){
-            if(slot[j]==-1){
-                slot[j]=i;  // eikhane ja mon chay dewa jabe
-                cntjob++;
-                totalprofit+=v[i].first;
-                break;
-            }
+        if(capacity==0){
+            break;
+        }
+        if(v[i].second.first<=capacity){
+            totalvalue +=v[i].second.second;
+            capacity-=v[i].second.first;         
+        }
+        else {
+            totalvalue += v[i].first*capacity;
+            break;
         }
     }
+    cout << "Total value in Knapsack = " << totalvalue << endl;
 }
